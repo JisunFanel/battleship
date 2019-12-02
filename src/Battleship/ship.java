9bec5e7da6516035fd083f7fac25ca73a47a3c17
JisunFanel <jisun.sabrina@gmail.com>
2019-12-02 16:28:28 +0100

    initial commit

---
+++ src/Battleship/ship.java
@@ -0,0 +1,16 @@
+package Battleship;
+
+public class Ship {
+	String modele;
+	String orientation;
+	String name;
+
+}
+
+public class Modeleship {
+	String carrier;
+	String battleship;
+	String cruiser;
+	String submarine;
+	String destroyer;
+}
